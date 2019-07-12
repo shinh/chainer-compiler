2ba3dc7810380d25ec461182885ef343551d8582
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-07-12 14:22:04 +0900

    Add a test for `print` with constant propagation

--- testcases/elichika_tests/syntax/Print.py
+++ testcases/elichika_tests/syntax/Print.py
@@ -8,6 +8,11 @@ class Print(chainer.Chain):
         return 0
 
 
+class PrintWithConstantPropagation(chainer.Chain):
+    def forward(self, x):
+        print('foo', 'test %d' % 3)
+        return 0
+
 # ======================================
 
 
@@ -16,6 +21,8 @@ import numpy as np
 
 def main():
     testtools.generate_testcase(Print, [10])
+    testtools.generate_testcase(PrintWithConstantPropagation, [10],
+                                subname='const_prop')
 
 if __name__ == '__main__':
     main()
